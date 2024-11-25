af3fc073d799f8021d6f18311dba14cfb0b93c34
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-11-25 15:40:10 +0700

    Add url and view class of each table

--- clinic_management/urls.py
+++ clinic_management/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.HomePageView.as_view(), name="home"),
+    path("payment/", views.PaymentView.as_view(), name="payment"),
+    path("treatment/", views.TreatmentView.as_view(), name="treatment"),
+    path("coverage-policy/", views.CoveragePolicyView.as_view(), name="coverage_policy"),
+    path("insurance/", views.InsuranceView.as_view(), name="insurance"),
+    path("patient/", views.PatientView.as_view(), name="patient"),
+    path("appointment/", views.AppointmentView.as_view(), name="appointment"),
+    path("diseases/", views.DiseasesView.as_view(), name="diseases"),
+    path("medical-record/", views.MedicalRecordView.as_view(), name="medical_record"),
+    path("medicine/", views.MedicineView.as_view(), name="medicine"),
+    path("medicine-record/", views.MedicineRecordView.as_view(), name="medicine_record"),
+    path("doctor/", views.DoctorView.as_view(), name="doctor"),
+    path("dosage/", views.DosageView.as_view(), name="dosage"),
+    path("availability/", views.AvailabilityView.as_view(), name="availability"),
 ]
--- clinic_management/views.py
+++ clinic_management/views.py
@@ -1,8 +1,65 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 
 # Create your views here.
 
-def home(request):
-    return render(request, "home.html")
+class HomePageView(generic.TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class PaymentView(generic.TemplateView):
+    template_name = "payment.html"
+
+
+class TreatmentView(generic.TemplateView):
+    template_name = "treatment.html"
+
+
+class CoveragePolicyView(generic.TemplateView):
+    template_name = "coverage_policy.html"
+
+
+class InsuranceView(generic.TemplateView):
+    template_name = "insurance.html"
+
+
+class PatientView(generic.TemplateView):
+    template_name = "patient.html"
+
+
+class AppointmentView(generic.TemplateView):
+    template_name = "appointment.html"
+
+
+class DiseasesView(generic.TemplateView):
+    template_name = "diseases.html"
+
+
+class MedicalRecordView(generic.TemplateView):
+    template_name = "medical_record.html"
+
+
+class MedicineView(generic.TemplateView):
+    template_name = "medicine.html"
+
+
+class MedicineRecordView(generic.TemplateView):
+    template_name = "medicine_record.html"
+
+
+class DoctorView(generic.TemplateView):
+    template_name = "doctor.html"
+
+
+class DosageView(generic.TemplateView):
+    template_name = "dosage.html"
+
+
+class AvailabilityView(generic.TemplateView):
+    template_name = "availability.html"
